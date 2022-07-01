e44efb30e5ceec8f24a080fd125a517689033b4f
P‌яα∂єєp‌ <96743949+its-Pradeep@users.noreply.github.com>
2022-07-02 00:29:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,7 +53,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
       #  await message.reply_photo(
        #     photo=random.choice(PICS),
-            await message.reply(script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+         await message.reply(script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
