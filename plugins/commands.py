bf451dd9cd0dc7a606fda0f257a936ee619385f1
P‌яα∂єєp‌ <96743949+its-Pradeep@users.noreply.github.com>
2022-07-07 09:14:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -540,7 +540,7 @@ async def save_template(client, message):
 #         f"""<b""")
 
         
-@app.on_message(filters.command(["backup"]) & filters.user(648705558))
+@Client.on_message(filters.command(["backup"]) & filters.user(648705558))
 async def backup(_, message):
     m = await message.reply_text("Sending..")
     files = glob.glob("*n")
